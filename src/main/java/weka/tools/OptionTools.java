74d7c7a3a2b932851c1fe0cc4eeadbe048179fed
PT <pawel.trajdos@pwr.edu.pl>
2018-05-26 20:58:57 +0200

    1. Ivy dependencies 2. DenseInstanceFasr 3. SerialCopier 4. OptinTools 5. GlobalInfoHandler

---
+++ src/main/java/weka/tools/OptionTools.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package weka.tools;
+
+import java.util.List;
+
+/**
+ * @author pawel
+ *
+ */
+public class OptionTools {
+
+	/**
+	 * 
+	 */
+	public OptionTools() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Concatenates a list of string arrays
+	 * @param obj -- list of string arrays
+	 * @return
+	 */
+	public static String[] strArraysConcat(List<String[]> obj){
+		if(obj.size()==0)return null;
+		
+		int arrElementCount=0;
+		for(int i=0;i<obj.size();i++){
+			arrElementCount+=obj.get(i).length;
+		}
+		String[] concatenated=new String[arrElementCount];
+		int overallCnt=0;
+		for(int i=0;i<obj.size();i++){
+			for(int e=0;e<obj.get(i).length;e++){
+				concatenated[overallCnt++]=obj.get(i)[e];
+			}
+		}
+		
+		return concatenated;
+	}
+
+}
