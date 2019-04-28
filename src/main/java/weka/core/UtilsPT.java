f090fe9e1d6fea77f2a98df3410ece64feae7e2e
PT <pawel.trajdos@pwr.edu.pl>
2019-04-28 08:38:30 +0200

    1. UtilsPT -- some statistical operations.

--- src/main/java/weka/core/UtilsPT.java
+++ src/main/java/weka/core/UtilsPT.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 /**
  * @author pawel trajdos
+ * @since 0.2.0
  * @version 0.2.0
  *
  */
@@ -33,6 +34,7 @@ public class UtilsPT {
 	}
 	/**
 	 * Calculated the truncated mean for the given array
+	 * @since 0.3.0
 	 * @version 0.3.0
 	 * @param array -- array to calculate the truncated means
 	 * @return truncated mean
@@ -59,8 +61,69 @@ public class UtilsPT {
 		
 		sum-= (minVal+maxVal);
 		double mean = sum/(array.length-2);
-		
 		return mean;
 	}
+	
+	/**
+	 * Calculate variance of the sample (array)
+	 * @since 0.4.0
+	 * @version 0.4.0
+	 * @param array
+	 * @return
+	 */
+	public static double var(double[] array) {
+		double variance =0;
+		double mean = Utils.mean(array);
+		for(int i=0;i<array.length;i++)
+			variance += (array[i] - mean)*(array[i] - mean);
+		variance/= array.length-1;
+		return variance;
+	}
+	
+	/**
+	 * Calculates standard deviation of the sample (array)
+	 * @since 0.4.0
+	 * @version 0.4.0
+	 * @param array
+	 * @return
+	 */
+	public static double stdDev(double[] array) {
+		return Math.sqrt(UtilsPT.var(array));
+	}
+	/**
+	 * Calculate covariance of two samples of equal length (arr1, arr2)
+	 * @since 0.4.0
+	 * @version 0.4.0
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 * @throws Exception when samples are incompatible
+	 */
+	public static double cov(double[] arr1, double[] arr2)throws Exception{
+		if(arr1.length != arr2.length)throw new Exception("Incompatible arrays");
+		double cov =0;
+		double m1 = Utils.mean(arr1);
+		double m2 = Utils.mean(arr2);
+		for(int i=0;i<arr1.length;i++)
+			cov+= (arr1[i] - m1)*(arr2[i]-m2);
+		cov /= arr1.length-1;
+		return cov;
+	}
+	
+	/**
+	 * Calculate correlation of two samples (arrays)
+	 * @since 0.4.0
+	 * @version 0.4.0
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 * @throws Exception when arrays are incompatible
+	 */
+	public static double corr(double[] arr1, double[] arr2)throws Exception{
+		double corr=cov(arr1,arr2);
+		corr/= stdDev(arr1)*stdDev(arr2);
+		return corr;
+	}
+	
 
 }
--- src/test/java/weka/core/UtilsPTTest.java
+++ src/test/java/weka/core/UtilsPTTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class UtilsPTTest {
 
 	@Test
-	public void test() {
+	public void testTmean() {
 		double[] array = new double[] {1,2,3,4,5};
 		double tmean = UtilsPT.truncatedMean(array);
 		assertTrue(Utils.eq(tmean, 3.0));
@@ -15,6 +15,36 @@ public class UtilsPTTest {
 		double[]array2 = new double[] {2,3};
 		tmean =  UtilsPT.truncatedMean(array2);
 		assertTrue(Utils.eq(tmean, 2.5));
+	}
+	@Test
+	public void testVars() {
+		double[] array = new double[] {1,2,3,4,5,6,7,8,9,10};
+		double[] array2 = new double[] {10,9,8,7,6,5,4,3,2,1};
+		double var = UtilsPT.var(array);
+		double desVar =9 + 1.0/6.0 ;
+		assertEquals(desVar, var, 10e-6);
+		
+		double sDev = UtilsPT.stdDev(array);
+		assertEquals(Math.sqrt(desVar), sDev, 10e-6);
+		
+		double cVar = 0;
+		try {
+			cVar = UtilsPT.cov(array, array2);
+		} catch (Exception e) {
+			fail("Exception caught");
+			e.printStackTrace();
+		}
+		assertEquals(-desVar, cVar, 10e-6);
+		
+		double corr =0;
+		try {
+			 corr = UtilsPT.corr(array, array2);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(-1, corr, 10e-6);
+		
 	}
 
 }
