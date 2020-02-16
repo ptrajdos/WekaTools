212f6061213b9f416bb78386958e54d508a2936b
pawel <pawel.trajdos@pwr.edu.pl>
2020-02-16 19:53:50 +0100

    1. WekaGOEChecker + tests

---
+++ src/test/java/weka/tools/tests/WekaGOECheckerTest.java
@@ -0,0 +1,38 @@
+package weka.tools.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import weka.classifiers.trees.J48;
+
+public class WekaGOECheckerTest {
+
+	@Test
+	public void testGlobalInfo() {
+		
+		WekaGOEChecker checker = new WekaGOEChecker();
+		checker.setObject(new J48());
+		
+		
+		boolean result = checker.checkCallGlobalInfo();
+		assertTrue("Class has GlobalInfo", result);
+		
+		
+		checker.setObject("XXX");
+		result = checker.checkCallGlobalInfo();
+		assertFalse("No global info", result);
+	}
+	
+	@Test 
+	public void testTipTexts() {
+		WekaGOEChecker checker = new WekaGOEChecker();
+		checker.setObject(new J48());
+		
+		boolean result = checker.checkToolTipsCall();
+		assertTrue("All tip text have been called properly.", result);
+		
+		
+	}
+
+}
